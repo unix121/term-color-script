8cb2eecf2d65132a4ef153a669f8fbbeca8f8572
unix121 <unix121@protonmail.com>
2018-01-04 22:52:50 +0200

    First commit

---
+++ sysinfo-minimal.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Author : Unix121
+# GitHub : github.com/unix121
+# E-mail : unix121@protonmail.com
+# Description : Simple term color script
+
+printf -v color0 %b "\e[30m"
+printf -v color1 %b "\e[31m"
+printf -v color2 %b "\e[32m"
+printf -v color3 %b "\e[33m"
+printf -v color4 %b "\e[34m"
+printf -v color5 %b "\e[35m"
+printf -v color6 %b "\e[36m"
+
+echo
+echo $color1 ███████$color2███████$color3███████$color4███████$color5███████$color6███████
+echo $color1 ███████$color2███████$color3███████$color4███████$color5███████$color6███████
+echo $color1 ███████$color2███████$color3███████$color4███████$color5███████$color6███████
+echo $color1 ███████$color2███████$color3███████$color4███████$color5███████$color6███████
+echo
